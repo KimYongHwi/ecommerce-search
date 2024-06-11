6de8deb7c8077e881d2385a7d32e77d693ba1c32
KimYongHwi <yhkim12389@gmail.com>
2024-06-12 01:14:23 +0900

    feat: add domain module

---
+++ domain/src/main/kotlin/kyh/ecommerce/search/domain/Product.kt
@@ -0,0 +1,14 @@
+package kyh.ecommerce.search.domain
+data class Product(
+    val id: Long,
+    val gender: String,
+    val mainCategory: String,
+    val subCategory: String,
+    val articleType: String,
+    val baseColor: String,
+    val season: String,
+    val year: String,
+    val usage: String,
+    val productDisplayName: String,
+    val image: String
+)
