6977a5f8bfc82fa61df2ca3f3d93775e8e581485
KimYongHwi <yhkim12389@gmail.com>
2024-06-15 23:36:38 +0900

    refactor: remove companion object

--- infrastructure/queue/kafka/src/main/kotlin/kyh/ecommerce/search/message/ProductMessage.kt
+++ infrastructure/queue/kafka/src/main/kotlin/kyh/ecommerce/search/message/ProductMessage.kt
@@ -1,7 +1,5 @@
 package kyh.ecommerce.search.message
 
-import kyh.ecommerce.search.domain.Product
-
 data class ProductMessage(
     val id: Long,
     val gender: String,
@@ -14,22 +12,4 @@ data class ProductMessage(
     val usage: String,
     val productDisplayName: String,
     val image: String
-) {
-    companion object {
-        fun toMessage(product: Product): ProductMessage {
-            return ProductMessage(
-                id = product.id,
-                gender = product.gender,
-                mainCategory = product.mainCategory,
-                subCategory = product.subCategory,
-                articleType = product.articleType,
-                baseColor = product.baseColor,
-                season = product.season,
-                year = product.year,
-                usage = product.usage,
-                productDisplayName = product.productDisplayName,
-                image = product.image
-            )
-        }
-    }
-}
+)
