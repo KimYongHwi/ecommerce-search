be9e3254809a547a8ba910e95e226fc8bb1ada26
KimYongHwi <yhkim12389@gmail.com>
2024-06-16 00:26:08 +0900

    refactor: updated http method

--- model-api/main.py
+++ model-api/main.py
@@ -1,10 +1,10 @@
 from io import BytesIO
 from typing import List
 
-import requests
 from PIL import Image
 from accelerate import Accelerator
 from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import CLIPProcessor, CLIPModel
 
 accelerator = Accelerator()
@@ -18,15 +18,22 @@ clip_model = CLIPModel.from_pretrained(model_id).to(device)
 app = FastAPI()
 
 
-@app.get("/features")
-async def get_features(item_nos: List[int], image_urls: List[str], texts: List[str]):
-    image_res = [requests.get(url) for url in image_urls]
-    images = [Image.open(BytesIO(res.content)).convert("RGB") for res in image_res]
+class GetFeatures(BaseModel):
+    item_ids: List[int]
+    images: List[str]
+    texts: List[str]
 
-    inputs = clip_processor(text=texts, images=images, return_tensors="pt", padding=True).to(device)
+
+@app.get("/features")
+async def get_features(param: GetFeatures):
+    images = [image for image in param.images]
+    images = [Image.open(BytesIO(image)).convert("RGB") for image in images]
+
+    inputs = clip_processor(text=param.texts, images=images, return_tensors="pt", padding=True).to(device)
     outputs = clip_model(**inputs)
 
     image_embeds = outputs.image_embeds.detach().cpu().numpy().tolist()
     text_embeds = outputs.text_embeds.detach().cpu().numpy().tolist()
 
-    return [{item_no: {'image_features': image_embeds[idx], 'text_features': text_embeds[idx]}} for idx, item_no in enumerate(item_nos)]
+    return [{item_no: {'image_features': image_embeds[idx], 'text_features': text_embeds[idx]}} for idx, item_no in
+            enumerate(param.item_nos)]
