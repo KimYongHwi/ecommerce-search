6f1d30f1d68604d4e899b9a5112cbe07f26d6fa2
KimYongHwi <yhkim12389@gmail.com>
2024-06-19 00:00:48 +0900

    refactor: add text encoder

--- model-api/main.py
+++ model-api/main.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from typing import List
 
 from PIL import Image
+import ast
 from accelerate import Accelerator
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -14,9 +15,9 @@ device = accelerator.device
 model_id = "patrickjohncyh/fashion-clip"
 
 clip_processor = CLIPProcessor.from_pretrained(model_id)
-clip_model = CLIPModel.from_pretrained(model_id).to(device) # 512
+clip_model = CLIPModel.from_pretrained(model_id).to(device)  # 512
 
-encoder_kor = LaserEncoderPipeline(lang="kor_Hang") # 1024
+text_encoder = LaserEncoderPipeline(lang="kor_Hang")  # 1024
 
 app = FastAPI()
 
@@ -29,19 +30,14 @@ class GetFeatures(BaseModel):
 
 @app.post("/features")
 async def get_features(param: GetFeatures):
-    images = [bytes(image, 'utf-8')[2:-1] for image in param.images]
-    images = [bytes(image) for image in images]
-    # images = [bytes([int(b, 16) for b in image.split(r'\x')[1:]]) for image in images]
+    images = [ast.literal_eval(image) for image in param.images]
+    images = [Image.open(BytesIO(image)).convert("RGB") for image in images]
 
-    # images = [bytes(image) for image in images]
-    return images
-    # images = [Image.open(BytesIO(bytes(image))).convert("RGB") for image in images]
-    #
-    # inputs = clip_processor(text=param.texts, images=images, return_tensors="pt", padding=True).to(device)
-    # outputs = clip_model(**inputs)
-    #
-    # image_embeds = outputs.image_embeds.detach().cpu().numpy().tolist()
-    # text_embeds = outputs.text_embeds.detach().cpu().numpy().tolist()
-    #
-    # return [{item_no: {'image_features': image_embeds[idx], 'text_features': text_embeds[idx]}} for idx, item_no in
-    #         enumerate(param.item_nos)]
+    inputs = clip_processor(text=param.texts, images=images, return_tensors="pt", padding=True).to(device)
+    outputs = clip_model(**inputs)
+
+    image_features = outputs.image_embeds.detach().cpu().numpy().tolist()
+    text_features = text_encoder.encode_sentences(param.texts)
+
+    return [{'item_id': item_id, 'image_features': image_features[idx], 'text_features': text_features[idx].tolist()}
+            for idx, item_id in enumerate(param.item_ids)]
