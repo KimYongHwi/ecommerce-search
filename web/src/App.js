49a5325b1ae947babee9c1a55030412e2997f430
KimYongHwi <yhkim12389@gmail.com>
2024-07-07 16:17:52 +0900

    feat: add react-router

--- web/src/App.js
+++ web/src/App.js
@@ -1,59 +1,25 @@
-import { useState } from 'react';
 import './App.css';
-import SearchForm from './SearchFrom';
-import ProductList from './ProductList';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
-import deliverVan from './assets/images/delivery-van.svg';
-import moneyBack from './assets/images/money-back.svg';
-import serviceHours from './assets/images/service-hours.svg';
-import NewArrival from './components/NewArrival';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Search from './pages/Search';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [total, setTotal] = useState(0);
-
   return (
     <body>
-      <Header setProducts={setProducts} setTotal={setTotal} />
+      <BrowserRouter>
+        <Header setProducts={setProducts} setTotal={setTotal} />
+        <Navbar />
 
-      <Navbar />
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/search/:keyword' element={<Search />}></Route>
+        </Routes>
 
-      <div class='container mx-auto'>
-        <Banner />
-      </div>
-
-      <div class='container py-16'>
-        <div class='w-10/12 grid grid-cols-1 md:grid-cols-3 gap-6 mx-auto justify-center'>
-          <div class='border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5'>
-            <img src={deliverVan} alt='Delivery' class='w-12 h-12 object-contain' />
-            <div>
-              <h4 class='font-medium capitalize text-lg'>Free Shipping</h4>
-              <p class='text-gray-500 text-sm'>Order over $200</p>
-            </div>
-          </div>
-          <div class='border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5'>
-            <img src={moneyBack} alt='Delivery' class='w-12 h-12 object-contain' />
-            <div>
-              <h4 class='font-medium capitalize text-lg'>Money Rturns</h4>
-              <p class='text-gray-500 text-sm'>30 days money returs</p>
-            </div>
-          </div>
-          <div class='border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5'>
-            <img src={serviceHours} alt='Delivery' class='w-12 h-12 object-contain' />
-            <div>
-              <h4 class='font-medium capitalize text-lg'>24/7 Support</h4>
-              <p class='text-gray-500 text-sm'>Customer support</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <NewArrival />
-
-      <Footer />
+        <Footer />
+      </BrowserRouter>
     </body>
   );
 }
--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
 import TextInput from './TextInput';
-import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function Header({ setProducts, setTotal }) {
+function Header() {
+  const navigate = useNavigate();
   const [formValue, setFormValue] = useState({ keyword: '' });
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    axios
-      .get(`http://localhost:8080/products/search?keyword=${formValue.keyword}&page=0&size=10`)
-      .then((res) => {
-        setProducts(res.data.products);
-        setTotal(res.data.total);
-      })
-      .catch((e) => {
-        console.error(e.message);
-      });
+    if (formValue.keyword != '') {
+      navigate(`/search/${formValue.keyword}`);
+    }
   }
 
   return (
@@ -63,78 +58,6 @@ function Header({ setProducts, setTotal }) {
       </div>
     </div>
   );
-
-  // return (
-  //   <header class='py-4 shadow-sm bg-white'>
-  //     <div class='container flex items-center justify-between'>
-  //       <a href='index.html'>
-  //         <img src='assets/images/logo.svg' alt='Logo' class='w-32' />
-  //       </a>
-
-  //       <div class='w-full max-w-xl relative flex'>
-  //         <span class='absolute left-4 top-3 text-lg text-gray-400'>
-  //           <i class='fa-solid fa-magnifying-glass'></i>
-  //         </span>
-  //         <input
-  //           type='text'
-  //           name='search'
-  //           id='search'
-  //           class='w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex'
-  //           placeholder='search'
-  //         />
-  //         <button class='bg-primary border border-primary px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex'>
-  //           Search
-  //         </button>
-  //       </div>
-
-  //       <div class='flex items-center space-x-4'>
-  //         <a href='#' class='text-center text-gray-700 hover:text-primary transition relative'>
-  //           <div class='text-2xl'>
-  //             <i class='fa-regular fa-heart'></i>
-  //           </div>
-  //           <div class='text-xs leading-3'>Wishlist</div>
-  //           <div class='absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs'>
-  //             8
-  //           </div>
-  //         </a>
-  //         <a href='#' class='text-center text-gray-700 hover:text-primary transition relative'>
-  //           <div class='text-2xl'>
-  //             <i class='fa-solid fa-bag-shopping'></i>
-  //           </div>
-  //           <div class='text-xs leading-3'>Cart</div>
-  //           <div class='absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs'>
-  //             2
-  //           </div>
-  //         </a>
-  //         <a href='#' class='text-center text-gray-700 hover:text-primary transition relative'>
-  //           <div class='text-2xl'>
-  //             <i class='fa-regular fa-user'></i>
-  //           </div>
-  //           <div class='text-xs leading-3'>Account</div>
-  //         </a>
-  //       </div>
-  //     </div>
-  //   </header>
-  // );
-
-  // return (
-  //   <header class='py-4 shadow-sm bg-white'>
-  //     <div class='container flex'>
-  //       <div class='w-full max-w-xl relative flex items-center justify-between'>
-  //         <input
-  //           type='text'
-  //           name='search'
-  //           id='search'
-  //           class='w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex'
-  //           placeholder='search'
-  //         />
-  // <button class='bg-primary border border-primary px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex'>
-  //   Search
-  // </button>
-  //       </div>
-  //     </div>
-  //   </header>
-  // );
 }
 
 export default Header;
